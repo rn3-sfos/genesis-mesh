e7a4239a1f6559bcc3b43f2d3857dc426adf0cf6
Akhil K <akhilkalwakurthy@gmail.com>
2024-11-06 05:38:51 +0000

    Initial commit

---
+++ reflection.py
@@ -0,0 +1,234 @@
+from langchain_core.tools import BaseTool
+from langchain_openai import ChatOpenAI
+from langchain_core.messages import HumanMessage, ToolMessage, SystemMessage
+from langchain_core.output_parsers.openai_tools import PydanticToolsParser
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.tools import StructuredTool
+from langgraph.prebuilt import ToolNode
+from langgraph.graph import END, StateGraph, START
+from langgraph.graph.message import add_messages
+
+from pydantic import BaseModel, Field, ValidationError
+from typing import Annotated, Type
+from typing_extensions import TypedDict
+import datetime
+from os import environ
+from uuid import uuid4
+
+from simple_agent import get_agent_response
+
+
+environ["OPENAI_API_KEY"] = "dummy"
+
+llm = ChatOpenAI(
+    model="codestral-22b-v0.1",
+    base_url="http://10.0.1.1:9080/v1",
+    streaming=True,
+    max_tokens=4096,
+    temperature=0.3,
+)
+
+
+class AgentSearchInput(BaseModel):
+    query: str = Field(description="query to look up on web")
+
+
+class AgentSearchTool(BaseTool):
+    name: str = "web_search_tool"
+    description: str = (
+        "A search engine."
+        "Useful for when you need to answer questions about current events."
+        "Input should be a search query."
+    )
+    args_schema: Type[BaseModel] = AgentSearchInput
+
+    def _run(self, query: str) -> str:
+        return get_agent_response(query)
+
+
+tool = AgentSearchTool()
+
+
+class Reflection(BaseModel):
+    missing: str = Field(description="Critique of what is missing.")
+    superfluous: str = Field(description="Critique of what is superfluous")
+
+
+class AnswerQuestion(BaseModel):
+    """Answer the question. Provide an extremely detailed answer, reflection, and then follow up with search queries to improve the answer."""
+
+    answer: str = Field(description="Extremely detailed answer to the question.")
+    reflection: Reflection = Field(description="Your reflection on the initial answer.")
+    search_queries: list[str] = Field(
+        description="1-3 search queries for researching improvements to address the critique of your current answer."
+    )
+
+
+class ResponderWithRetries:
+    def __init__(self, runnable, validator):
+        self.runnable = runnable
+        self.validator = validator
+
+    def respond(self, state: list):
+        response = []
+        for attempt in range(3):
+            response = self.runnable.invoke(
+                {"messages": state["messages"]}, {"tags": [f"attempt:{attempt}"]}
+            )
+            try:
+                self.validator.invoke(response)
+                return {"messages": response}
+            except ValidationError as e:
+                state = state + [
+                    response,
+                    ToolMessage(
+                        content=f"{repr(e)}\n\nPay close attention to the function schema.\n\n"
+                        + self.validator.schema_json()
+                        + " Respond by fixing all validation errors.",
+                        tool_call_id=response.tool_calls[0]["id"],
+                    ),
+                ]
+        return {"messages": response}
+
+
+actor_prompt_template = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """You are expert researcher.
+Current time: {time}
+
+1. {first_instruction}
+2. Reflect and critique your answer. Be severe to maximize improvement.
+3. Recommend search queries to research information and improve your answer.""",
+        ),
+        MessagesPlaceholder(variable_name="messages"),
+        (
+            "user",
+            "\n\n<system>Reflect on the user's original question and the"
+            " actions taken thus far. Respond using the {function_name} function.</reminder>",
+        ),
+    ]
+).partial(
+    time=lambda: datetime.datetime.now().isoformat(),
+)
+initial_answer_chain = actor_prompt_template.partial(
+    first_instruction="Write an extremely detailed answer.",
+    function_name=AnswerQuestion.__name__,
+) | llm.bind_tools(tools=[AnswerQuestion])
+validator = PydanticToolsParser(tools=[AnswerQuestion])
+
+first_responder = ResponderWithRetries(
+    runnable=initial_answer_chain, validator=validator
+)
+
+revise_instructions = """Revise your previous answer using the new information.
+    - You should use the previous critique to add important information to your answer.
+        - You MUST include numerical citations in your revised answer to ensure it can be verified.
+        - Add a "References" section to the bottom of your answer in form of:
+            - [1] https://example.com
+            - [2] https://example.com
+    - You should use the previous critique to remove superfluous information from your answer.
+    - Do not miss any details.
+"""
+
+
+# Extend the initial answer schema to include references.
+# Forcing citation in the model encourages grounded responses
+class ReviseAnswer(AnswerQuestion):
+    """Revise your original answer to your question. Provide an answer, reflection,
+    cite your reflection with references, and finally
+    add search queries to improve the blog."""
+
+    references: list[str] = Field(
+        description="Citations motivating your updated answer."
+    )
+
+
+revision_chain = actor_prompt_template.partial(
+    first_instruction=revise_instructions,
+    function_name=ReviseAnswer.__name__,
+) | llm.bind_tools(tools=[ReviseAnswer])
+revision_validator = PydanticToolsParser(tools=[ReviseAnswer])
+
+revisor = ResponderWithRetries(runnable=revision_chain, validator=revision_validator)
+
+
+def run_queries(search_queries: list[str], **kwargs):
+    """Run the generated queries."""
+    return tool.batch([{"query": query} for query in search_queries])
+
+
+tool_node = ToolNode(
+    [
+        StructuredTool.from_function(run_queries, name=AnswerQuestion.__name__),
+        StructuredTool.from_function(run_queries, name=ReviseAnswer.__name__),
+    ]
+)
+
+
+class State(TypedDict):
+    messages: Annotated[list, add_messages]
+
+
+MAX_ITERATIONS = 5
+builder = StateGraph(State)
+builder.add_node("draft", first_responder.respond)
+
+
+builder.add_node("execute_tools", tool_node)
+builder.add_node("revise", revisor.respond)
+# draft -> execute_tools
+builder.add_edge("draft", "execute_tools")
+# execute_tools -> revise
+builder.add_edge("execute_tools", "revise")
+
+# Define looping logic:
+
+
+def _get_num_iterations(state: list):
+    i = 0
+    for m in state[::-1]:
+        if m.type not in {"tool", "ai"}:
+            break
+        i += 1
+    return i
+
+
+def event_loop(state: list):
+    # in our case, we'll just stop after N plans
+    num_iterations = _get_num_iterations(state["messages"])
+    if num_iterations > MAX_ITERATIONS:
+        return END
+    return "execute_tools"
+
+
+# revise -> execute_tools OR end
+builder.add_conditional_edges("revise", event_loop, ["execute_tools", END])
+builder.add_edge(START, "draft")
+graph = builder.compile()
+
+user_input = input("User query: ")
+user_context = tool.invoke({"query": user_input})
+
+events = graph.stream(
+    {
+        "messages": [
+            SystemMessage(
+                content="""
+                - You are an AI content writer.
+                - Write an extremely detailed and complete answer for the user query.
+                - Make sure to include all relevant information.
+                - Note that the information should be specific to india or Axis Bank.
+                - You must use valid markdown syntax.
+                """
+            ),
+            ToolMessage(content=user_context, tool_call_id=str(uuid4())),
+            HumanMessage(content=user_input),
+        ]
+    },
+    stream_mode="values",
+)
+for i, step in enumerate(events):
+    print(f"Step {i}")
+    step["messages"][-1].pretty_print()
---
+++ simple_agent.py
@@ -0,0 +1,78 @@
+from langchain_openai.chat_models import ChatOpenAI
+from langgraph.prebuilt import create_react_agent
+from langchain_core.tools import tool
+from langchain_community.tools.searx_search.tool import SearxSearchResults
+from langchain_community.utilities.searx_search import SearxSearchWrapper
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_community.utilities.wikipedia import WikipediaAPIWrapper
+from langchain_community.tools.wikipedia.tool import WikipediaQueryRun
+from langchain_core.messages import SystemMessage, HumanMessage
+from typing import List
+from rich.console import Console
+from rich.markdown import Markdown
+from os import environ
+
+console = Console()
+
+environ["OPENAI_API_KEY"] = "dummy"
+
+llm = ChatOpenAI(
+    model="qwen2.5-instruct",
+    base_url="http://10.0.1.1:9080/v1",
+    streaming=True,
+    max_tokens=4096,
+    temperature=0.3,
+)
+
+searx_wrapper = SearxSearchWrapper(searx_host="http://10.0.1.1:9081")
+searx_tool = SearxSearchResults(verbose=False, wrapper=searx_wrapper, num_results=6)
+
+
+@tool
+def scrape_webpage(url: str) -> str:
+    """Use this to scrape the provided web page for detailed information.
+    Args:
+        url: a valid url to scrape
+    """
+    try:
+        loader = WebBaseLoader(f"https://r.jina.ai/{url}")
+        docs = loader.load()
+        return "\n\n".join(
+            [
+                f'<Document name="{doc.metadata.get("title", "")}" url="{doc.metadata.get("source", "")}">\n{doc.page_content}\n</Document>'
+                for doc in docs
+            ]
+        )
+    except Exception as e:
+        print(f"WARN: {str(e)}")
+        raise e
+
+
+tools = [
+    searx_tool,
+    scrape_webpage,
+    # WikipediaQueryRun(
+    #     api_wrapper=WikipediaAPIWrapper()
+    # )
+]
+
+
+graph = create_react_agent(model=llm, tools=tools)
+
+
+def get_agent_response(user_query: str) -> str:
+    response = graph.invoke(
+        input={
+            "messages": [
+                SystemMessage(
+                    content="""
+                        - Note that the information should be specific to india or Axis Bank.
+                        - Extensively research and create an extremely detailed blog post for user query. Add a 'References' section to the bottom of your answer in form of:
+                            - [1] https://example.com
+                            - [2] https://example.com"""
+                ),
+                HumanMessage(content=user_query),
+            ]
+        }
+    )
+    return response["messages"][-1].content
